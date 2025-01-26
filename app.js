c2c46f7b78a084bbc8c0b87e812ada78ea918387
Tejas Surse <118660087+TejasSurse@users.noreply.github.com>
2025-01-27 03:10:08 +0530

    Implement contact form and error page; update navigation links

--- app.js
+++ app.js
@@ -4,9 +4,35 @@ const mysql = require('mysql2');
 const path = require('path');
 const bodyParser = require('body-parser');
 const engine = require('ejs-mate');
-
+const nodeMailer = require("nodemailer");
+const { title } = require('process');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+// Transport Setup
+const transporter = nodeMailer.createTransport({
+  secure : true,
+  host : "smtp.gmail.com",
+  port : 465,
+  auth : {
+    user : process.env.EMAIL_ID,
+    pass : process.env.EMAIL_PASS
+    },
+});
+
+function sendMail(to, sub, msg){
+  return transporter.sendMail({
+    from : "DreamCraft.Dev App",
+    to,
+    subject : sub,
+    html : msg,
+  })
+}
+
+
+
+
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -53,10 +79,39 @@ app.get("/about", (req, res)=>{
 });
 
 
+
+// contact form
+
+app.get("/contact", (req, res)=>{
+    res.render('listings/contact');
+});
+
+app.post("/contact", async(req, res) => {
+  const { name, number, email, message } = req.body;
+
+  const emailContent = `
+   <div style="font-family: Arial, sans-serif; color: #333; line-height: 1.6;">
+      <h2>New Enquiry Received</h2>
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Phone:</strong> ${number}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Message:</strong> ${message}</p>
+    </div>
+  `;
+  try {
+    await sendMail("tejas.s.surse2004@gmail.com", "NEW ENQUIRY", emailContent);
+    res.render('listings/contact', { message: "Email sent successfully" });
+  } catch (err) {
+    console.log(err);
+    res.render("listings/error", { title: "Failed to send enquiry. Please try again later." });
+  }
+});
+
+
 // underconstruction
 app.use((req, res) => {
     res.status(404).render('listings/404');
-  });
+});
 
 
 
